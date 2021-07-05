ceaafcc06894bbc99d2f7e46a2263e1e9f18202f
bayan algousani <bayanalgousani@gmail.com>
2021-07-05 15:29:43 +0300

    edit order

--- database/migrations/2021_05_18_133735_create_orders_table.php
+++ database/migrations/2021_05_18_133735_create_orders_table.php
@@ -16,21 +16,21 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->enum('status',['ORDERED','SHIPPED','PACKED'])->nullable();
-<<<<<<< HEAD
-            $table->dateTime('order_date')->nullable();
-=======
+            
+            $table->double('ordering_quantity')->nullable();
             $table->date('order_date')->nullable();
-            $table->date('shipped_date')->nullable();
+
+            $table->boolean('is_packed')->nullable();
+            $table->double('packing_quantity')->nullable(); 
             $table->date('packed_date')->nullable();
 
->>>>>>> 72616b99c3a77d215317351453483e4faabff97b
-            $table->double('ordering_quantity')->nullable();
-            $table->boolean('packed')->nullable();
-            $table->double('packing_quantity')->nullable();
-            $table->dateTime('packed_date')->nullable();
-            $table->boolean('Haulage')->nullable(); 
-            $table->double('shipping_quantity')->nullable();
-            $table->dateTime('shipped_date')->nullable();
+            $table->boolean('is_shipped')->nullable(); 
+            $table->double('shipping_quantity')->nullable(); 
+            $table->date('shipped_date')->nullable();
+
+            $table->boolean('is_done')->nullable(); 
+            $table->boolean('is_available')->nullable(); 
+
             $table->double('price')->nullable();
             $table->bigInteger('customer_id')->unsigned();
             $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
